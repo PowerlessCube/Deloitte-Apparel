d4df45e7f69a3c68fc794de70741ad98c21569d7
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-08-02 10:56:55 +0100

    added basic form elements to apparel items

--- client/src/components/ApparelDisplay.jsx
+++ client/src/components/ApparelDisplay.jsx
@@ -8,7 +8,11 @@ var ApparelDisplay = React.createClass ({
 					<div key={index} className="apparel-item">
 						<li className="label">{apparel.name}</li>
 						<li><span className="label">Price: </span>£{apparel.price.toFixed(2)}</li>
-						<li><span className="label">Quantity: </span>{apparel.quantity}</li>
+						<li><span className="label">Qty: </span>{apparel.quantity}</li>
+						<input type="button" value="-"/>
+						<input type="text" value="0"/>
+						<input type="button" value="+"/>
+						<input type="button" value="Add to Basket"/>
 					</div>
 				)
 			})
